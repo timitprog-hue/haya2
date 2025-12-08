c139f9ca59f36d5452145ff6830ece3382758518
sandy39as <sandyramdani65@gmail.com>
2025-12-08 08:57:44 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,26 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Front\HomeController;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/services', [HomeController::class, 'services'])->name('services');
-Route::get('/projects', [HomeController::class, 'projects'])->name('projects');
-Route::get('/about', [HomeController::class, 'about'])->name('about');
-Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
+// ===== Halaman utama (one page) =====
+Route::view('/', 'front.home')->name('home');
+Route::view('/about', 'front.about')->name('about');
+Route::view('/services', 'front.services')->name('services');
+Route::view('/projects', 'front.projects')->name('projects');
+Route::view('/contact', 'front.contact')->name('contact');
 
-// Switch bahasa (ID / EN)
+// ===== Switch bahasa (ID / EN) =====
 Route::get('/lang/{locale}', function ($locale) {
     if (in_array($locale, ['id', 'en'])) {
         session(['locale' => $locale]);
     }
     return back();
 })->name('lang.switch');
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/services', [HomeController::class, 'services'])->name('services');
+Route::get('/projects', [HomeController::class, 'projects'])->name('projects');
+Route::get('/about', [HomeController::class, 'about'])->name('about');
+Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
 
 Route::get('/', function () {
     return view('front.home');
